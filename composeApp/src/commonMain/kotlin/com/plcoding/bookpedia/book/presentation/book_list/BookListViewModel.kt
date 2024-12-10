384b062ded30cf000bda0a713a1e3c9e14ccd5a2
ernisernis <ernestas.dainys@gmail.com>
2024-12-10 22:52:08 +0200

    Implement basic book list structure (Action, State, ViewModel classes)

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListViewModel.kt
@@ -0,0 +1,35 @@
+package com.plcoding.bookpedia.book.presentation.book_list
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.selects.select
+
+class BookListViewModel: ViewModel() {
+
+    private val _state = MutableStateFlow(BookListState())
+    val state = _state.asStateFlow()
+
+    fun onAction(action: BookListAction) {
+        when (action) {
+            is BookListAction.OnBookClick -> {
+
+            }
+            is BookListAction.OnSearchQueryChange -> {
+               _state.update {
+                   it.copy(
+                       searchQuery = action.query
+                   )
+               }
+            }
+            is BookListAction.OnTabSelected -> {
+               _state.update {
+                   it.copy(
+                       selectedTabIndex = action.index
+                   )
+               }
+            }
+        }
+    }
+}
